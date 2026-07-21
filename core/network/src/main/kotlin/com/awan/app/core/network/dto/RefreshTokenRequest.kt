11eadd066a9f8343f70e58534b26b2967184f589
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-07-21 21:36:53 +0300

    refactor(network): split NetworkDtos into independent files

---
+++ core/network/src/main/kotlin/com/awan/app/core/network/dto/RefreshTokenRequest.kt
@@ -0,0 +1,10 @@
+package com.awan.app.core.network.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RefreshTokenRequest(
+    @SerialName("refreshToken") val refreshToken: String,
+    @SerialName("deviceId") val deviceId: String,
+)
