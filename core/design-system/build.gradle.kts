6baa54d0166110a9ad98c5c13d841371d6a997bb
esraaehab333 <esraaehabb333@gmail.com>
2026-07-29 04:54:46 +0300

    feature : add error awan error snackbar

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
 
 
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
