1c120343ab808efe6feb37f50fdf75a94f91f600
esraaehab333 <esraaehabb333@gmail.com>
2026-08-09 03:04:23 +0300

    add update session params

---
+++ core/model/src/main/kotlin/com/awan/app/core/model/UpdateSessionParams.kt
@@ -0,0 +1,10 @@
+package com.awan.app.core.model
+
+import java.time.LocalDateTime
+
+data class UpdateSessionParams(
+    val start: LocalDateTime? = null,
+    val end: LocalDateTime? = null,
+    val status: SessionStatus? = null,
+    val locked: Boolean? = null
+)
